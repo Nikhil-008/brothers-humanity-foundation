9130b5dc0702eaca20d676c62394c73a7da99fed
NiKkhiL022 <nikhil@able.do>
2024-10-25 10:45:44 +0530

    chore: adds project setup

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+	// export let link, name;
+</script>
+
+<div class="max-w-full px-10 flex gap-5 justify-between items-center mt-10">
+	<a href="/">
+		<img src="/images/logo.jpeg" alt="" class="h-[80px]" />
+	</a>
+	<div class="flex gap-10 items-center uppercase">
+		<a href="/">HOME</a>
+		<a href="/">About Us</a>
+		<a href="/">Contact Us</a>
+		<a href="/">Gallery</a>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Brothers Humanity Foundation</title>
+</svelte:head>
+<slot />
